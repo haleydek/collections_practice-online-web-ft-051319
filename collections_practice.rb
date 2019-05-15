90ff2386d6f22f58269ceaa13335ee6557daf9ad
Haley DeKeyser <hcd09@live.com>
2019-05-15 13:11:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+require "pry"
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.collect do |string|
+    string.length
+  end
+end
+    
+        
+      
